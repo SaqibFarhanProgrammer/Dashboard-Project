43ad428f7e32bd0978c225cd6ce68b49d0960d0c
saqib <saqibkhatai@gmail.com>
2025-09-04 20:50:19 -0700

    bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,21 +18,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={poppins.className} suppressHydrationWarning>
         <div className="flex">
-          {/* Sidebar */}
           <aside className="fixed top-0 left-0 w-64 h-screen z-50 bg-white shadow">
             <Navbar />
           </aside>
-
-          {/* Content wrapper */}
           <div className="flex-1 flex flex-col ml-64">
-            {/* Top Navbar */}
             <header className="fixed top-0 left-64 right-0 h-16 z-40 bg-white shadow flex items-center">
               <TopNavbar />
             </header>
-
-            {/* Main content */}
             <main className="flex-1 p-6 pt-20 bg-gray-50 min-h-screen">
               {children}
             </main>
